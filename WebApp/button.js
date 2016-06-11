9b1b5b9077a8c6ed89961af892d7d2b3c32bd328
Shuangshuang Zhao <poppindouble@gmail.com>
2016-06-11 14:42:16 -0400

    fixed merge error

---
+++ WebApp/button.js
@@ -0,0 +1,54 @@
+
+// for buttons
+function uploadImageClicked() {
+	console.log("upload image clicked");
+}
+
+function colourItClicked() {
+	console.log("colour image clicked");
+}
+
+
+
+// for sliders
+
+// vibrance
+function vibranceSliderOnChange(newValue) {
+	console.log(newValue);
+}
+
+function vibranceSliderChanging(newValue) {
+	console.log(newValue);	
+}
+
+// hue
+
+function hueSliderOnChange(newValue) {
+
+}
+
+
+function hueSliderChanging(newValue) {
+
+}
+
+// saturation
+
+function saturationSliderOnChange(newValue) {
+
+}
+
+function saturationSliderChanging(newValue) {
+
+}
+
+// colour sample rate
+
+function colourSampleRateSliderOnChange(newValue) {
+
+}
+
+function colourSampleRateSliderChanging(newValue) {
+
+}
+
