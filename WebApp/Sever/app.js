b88d459530a30af203c5db327d245fe5eb4ec08d
Shuangshuang Zhao <poppindouble@gmail.com>
2016-06-11 20:49:52 -0400

    Get app.js ready to run the colour script

--- WebApp/Sever/app.js
+++ WebApp/Sever/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 var bodyParser = require('body-parser');
+var shell = require('shelljs');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
@@ -20,15 +21,20 @@ app.get('/', function (req, res) {
 
 app.post('/write', function (req, res) {
 	var img = req.body.imgData
-
-  console.log(img)
-
-
 	var data = img.replace(/^data:image\/\w+;base64,/, "");
 	var buf = new Buffer(data, 'base64');
-	fs.writeFile('image.png', buf);
+	fs.writeFile('image.png', buf, function(error) {
+    if(error) {
+      console.log("we have error")
+    } else {
+      console.log("call the script")
+      
+    }
+  });
 
-  	res.send('POST request to the homepage');
+
+
+  res.send('POST request to the homepage');
 });
 
 
